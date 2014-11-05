d47c60da43e5c46a88178d408a533715b6662981
Jay Deuskar <coffeejay@me.com>
2014-11-04 22:22:34 -0500

    Enforce post URL uniqueness.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -13,7 +13,11 @@ Template.postSubmit.events({
  if (error)
  	return alert(error.reason);
  
- Router.go('postPage', {_id: result._id}); 
+ // show this result but route anyway
+ if (result.postExists)
+ 	alert('This link has already been posted');
+ 
+ Router.go('postPage', {_id: result._id});  
  });
 
  }
